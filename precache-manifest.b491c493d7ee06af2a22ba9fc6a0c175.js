b639bdd2d2237450097d6f765a0e18a999275d88
Jaynarol <jaynarol@gmail.com>
2019-06-28 11:54:45 +0700

    Updates

---
+++ precache-manifest.b491c493d7ee06af2a22ba9fc6a0c175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dd8d23aee7d126bfda52bb738bdb48",
+    "url": "/jtxbody/index.html"
+  },
+  {
+    "revision": "be9de9380ade52e5a724",
+    "url": "/jtxbody/static/css/main.06f6715c.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/jtxbody/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "be9de9380ade52e5a724",
+    "url": "/jtxbody/static/js/main.05d74a94.chunk.js"
+  },
+  {
+    "revision": "487217f7ec902cc6fddc",
+    "url": "/jtxbody/static/js/runtime~main.3fbc751a.js"
+  }
+]);
